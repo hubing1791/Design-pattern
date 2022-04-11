845563d566774d1f8c2621eff593dd196511d469
hubing1791 <benrenbucun@qq.com>
2022-04-11 09:15:00 +0800

    2022-04-11

---
+++ src/singleton/enumsingleton/EnumSingleton.java
@@ -0,0 +1,8 @@
+package singleton.enumsingleton;
+
+public enum EnumSingleton {
+    INSTANCE;
+    public void print(){
+        System.out.println(this.hashCode());
+    }
+}
---
+++ src/singleton/innerclass/InnerClassSingleton.java
@@ -0,0 +1,16 @@
+package singleton.innerclass;
+
+public class InnerClassSingleton {
+    private  static  class  InnerClassHolder{
+        public static InnerClassSingleton instance = new InnerClassSingleton();
+    }
+    private InnerClassSingleton(){
+        //防止利用反射加载多个实例
+        if (InnerClassHolder.instance!= null){
+            throw new RuntimeException("单例不允许多个实例");
+        }
+    }
+    public static InnerClassSingleton getInstance(){
+        return InnerClassHolder.instance;
+    }
+}
